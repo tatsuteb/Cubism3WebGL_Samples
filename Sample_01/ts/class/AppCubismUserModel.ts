9a013c0619e2a41faf4be0ae781951a55d61c6e6
DESKTOP-B7036PK\tatsuteb <tatsuteb@gmail.com>
2018-10-09 02:15:47 +0900

    canvas、gl、frameworkの初期化 modelの派生クラスを作成

---
+++ Sample_01/ts/class/AppCubismUserModel.ts
@@ -0,0 +1,11 @@
+import { Live2DCubismFramework as cubismusermodel} from '../../../Framework/model/cubismusermodel';
+import CubismUserModel = cubismusermodel.CubismUserModel;
+
+
+export default class AppCubismUserModel extends CubismUserModel {
+
+    constructor() {
+        super();
+    }
+
+}
--- Sample_01/ts/main.ts
+++ Sample_01/ts/main.ts
@@ -1,1 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {});
+import { Live2DCubismFramework } from '../../Framework/live2dcubismframework';
+import CubismFramework = Live2DCubismFramework.CubismFramework;
+
+import AppCubismUserModel from './class/AppCubismUserModel';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    /**
+     * Canvasの初期化
+     */
+
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    canvas.width = 512;
+    canvas.height = 512;
+
+    /**
+     * WebGLコンテキストの初期化
+     */
+
+    const gl = canvas.getContext('webgl') || canvas.getContext("experimental-webgl");
+
+    if (gl === null) return alert("WebGL未対応のブラウザです。");
+
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    gl.clearColor(0.0, 0.0, 0.0, 0.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+
+    /**
+     * Frameworkの初期化
+     */
+
+     CubismFramework.startUp();
+     CubismFramework.initialize();
+
+     /**
+      * Live2Dモデルの初期化
+      */
+
+      const model = new AppCubismUserModel();
+    //   const model = new ModelBuilder()
+    //     .SetupModel(moc)
+    //     .SetupTexture(texture[])
+    //     .build();
+
+});
